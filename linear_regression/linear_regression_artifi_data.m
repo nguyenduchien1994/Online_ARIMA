e8403a7a83e72fa1467d0610948214e5aaddbebb
yaqinh <yaqinh@bu.edu>
2017-12-08 18:47:50 -0600

    artifi data LR added

---
+++ linear_regression/linear_regression_artifi_data.m
@@ -0,0 +1,57 @@
+%Linear Regression
+%Load data from real artifical data
+data_y = load('artificial_data.mat');
+data_y = (data_y.data_d0)';
+data_x = (1:1:size(data_y, 1))';
+
+%We choose about two thirds as our train data and the rest to be test data
+train_size = round(size(data_y, 1) * 0.7);
+xtrain = data_x(1:train_size, 1);
+ytrain = data_y(1:train_size, 1);
+xtest = data_x(train_size + 1:end, 1);
+ytest = data_y(train_size + 1:end, 1);
+
+%Use fitlm() to build the linear model
+mdl = fitlm(xtrain, ytrain, 'poly9', 'RobustOpts', 'on')
+%Get coefficient from the fitlm model
+%estimate_1 = 1 + (-0.0011825) * xtrain + (2.4641e-09) * xtrain .^2;
+estimate_1_train = 1 + (9.5588e-15) * xtrain .^6 + (-1.8643e-17) * xtrain .^7 + (1.2087e-20) * xtrain .^8 + (-2.5514e-24) * xtrain .^9;
+estimate_1_test = 1 + (9.5588e-15) * xtest .^6 + (-1.8643e-17) * xtest .^7 + (1.2087e-20) * xtest .^8 + (-2.5514e-24) * xtest .^9;
+MSE_fitlm_train = (sum((estimate_1_train - ytrain) .^ 2))^(0.5)/size(ytrain, 1)
+MSE_fitlm_test = (sum((estimate_1_test - ytest) .^ 2))^(0.5)/size(ytest, 1)
+
+%Use robustfit() to build the linear model
+%Get coefficient from the robust fit 
+rob_coeff = robustfit(xtrain, ytrain);
+estimate_2_train = rob_coeff(1) + rob_coeff(2) * xtrain;
+estimate_2_test = rob_coeff(1) + rob_coeff(2) * xtest;
+MSE_robustfit_train = (sum((estimate_2_train - ytrain) .^ 2))^(0.5)/size(ytrain, 1)
+MSE_robustfit_test = (sum((estimate_2_test - ytest) .^ 2))^(0.5)/size(ytest, 1)
+
+%Use regress() to build the linear model
+b = regress(ytrain, xtrain);
+estimate_3_train = b * xtrain;
+estimate_3_test = b * xtest;
+MSE_regress_train = (sum((estimate_3_train - ytrain) .^ 2))^(0.5)/size(ytrain, 1)
+MSE_regress_test = (sum((estimate_3_test - ytest) .^ 2))^(0.5)/size(ytest, 1)
+
+%Plot the train dand test ata and estimates
+figure(1);
+scatter(xtrain, ytrain);
+hold on;
+plot(xtrain, estimate_1_train, xtrain, estimate_2_train , xtrain, estimate_3_train);
+hold off;
+title('Artificial train data Time Series Linear Regression Plot on train data');
+leg1 = legend('Artificial train data', 'fitlm() prediction', 'robustfit() prediction', 'regress() prediction');
+xlabel('Index');
+ylabel('Artificial data');
+
+figure(2);
+scatter(xtest, ytest);
+hold on;
+plot(xtest, estimate_1_test, xtest, estimate_2_test , xtest, estimate_3_test);
+hold off;
+title('Artificial test data Time Series Linear Regression Plot on test data');
+leg1 = legend('Artificial test data', 'fitlm() prediction', 'robustfit() prediction', 'regress() prediction');
+xlabel('Index');
+ylabel('Artificial data');
---
+++ linear_regression/linear_regression_class_artifi_data.m
@@ -0,0 +1,96 @@
+%Linear Regression using method taught in class
+%We do not use degree = 1 equation because obviously it is too simple for
+%Bitcoin data
+
+%Load data from aritifical data
+data_y = load('artificial_data.mat');
+data_y = (data_y.data_d0)';
+data_x = (1:1:size(data_y, 1))';
+
+%We choose about two thirds as our train data and the rest to be test data
+train_size = round(size(data_y, 1) * 0.7);
+xtrain = data_x(1:train_size, 1);
+ytrain = data_y(1:train_size, 1);
+xtest = data_x(train_size + 1:end, 1);
+ytest = data_y(train_size + 1:end, 1);
+
+%First step, determine the poly degree in our model
+max_deg = 30;
+[fits_train, fits_test, mses_train, mses_test, best_deg] = find_best_degree(xtrain, ytrain, xtest, ytest, max_deg);
+
+best_deg_train = best_deg(1)
+best_deg_test = best_deg(2)
+
+regress_d2 = fits_train(:, 2);
+regress_d4 = fits_train(:, max_deg);
+regress_d10 = fits_train(:, round(max_deg/2));
+regress_d14 = fits_train(:, round(max_deg/3));
+regress_best_train = fits_train(:, best_deg_train);
+regress_best_test = fits_train(:, best_deg_test);
+
+%Plot the prediction and origin data
+figure(1);
+scatter(xtrain, ytrain);
+hold on
+plot(xtrain, regress_d2, xtrain, regress_d4, xtrain, regress_d10, xtrain, regress_d14, xtrain, regress_best_train, xtrain, regress_best_test);
+hold off
+title('Results for polynomial fits with various degrees in train data');
+legend('origin data', 'degree = 2', ['degree = ', num2str(max_deg)], ['degree = ', num2str(round(max_deg/2))], ['degree = ', num2str(round(max_deg/3))], 'best train degree',  'best test degree');
+xlabel('date');
+ylabel('Artificial data');
+
+%Plot the MSE for train data and test data with degreee from 1 to
+%Max_degree
+figure(2);
+plot((1:1:max_deg)', mses_train(:), (1:1:max_deg)', mses_test(:));
+title(['MSE for training data and test data with polynomial degree from 1 to ', num2str(max_deg)]);
+legend('MSE for training data', 'MSE for testing data');
+xlabel('degree');
+ylabel('MSE');
+
+%Second step, since we found the best degree for train data and test data, then we need to find the
+%In(namida) with L2 regularization for the best deg train and best deg test
+best_deg = [best_deg_train; best_deg_test];
+min_k = -25;
+max_k = 25;
+ek = min_k:1:max_k;
+
+for i = 1:1:2 
+    [fits_train, fits_test, mses_train, mses_test, best_k] = find_best_Ink(xtrain, ytrain, xtest, ytest, best_deg(i), min_k, max_k);
+    best_k_value = exp(best_k(i))
+
+    %(Plot MSE for train data and test data with best degree and different values of In(namida)
+    figure(3);
+    subplot(2, 1, i);
+    plot((ek)', mses_train(:), (ek)', mses_test(:));
+    title(['MSE for training data and test data with polynomial degree = ', num2str(best_deg(i)) ,' In(k) from ', num2str(ek(1)),' to ', num2str(ek(end))]);
+    legend('MSE for training data', 'MSE for testing data');
+    xlabel('In(k)');
+    ylabel('MSE');
+
+    %Compare the result for non-regularized and L2-regularized fit
+    fit_L2_min = fits_test(:, best_k(i));
+    figure(4);
+    subplot(2, 1, i);
+    scatter(xtest, ytest);
+    hold on
+    plot(xtest, fits_test(:, best_deg(i)), xtest, fit_L2_min);
+    hold off
+    title(['Original degree poly degree = ', num2str(best_deg(i)), 'fit vs L2-regularized degree = ', num2str(best_k_value), ' fit with smallest MSE']);
+    legend('test data', 'original poly fit', 'L2-regularized fit with smallest MSE');
+    xlabel('date');
+    ylabel('Artificial data');
+
+
+    %Plot the result for test data L2-regularized fit with best degree and best In(k) 
+    figure(5);
+    subplot(2, 1, i);
+    scatter(xtest, ytest);
+    hold on
+    plot(xtest, fit_L2_min);
+    hold off
+    title(['L2-regularized degree = ', num2str(best_deg(i)), ' In(k) = ', num2str(best_k(i)), ' fit with smallest MSE']);
+    legend('test data', 'L2-regularized fit with smallest MSE');
+    xlabel('date');
+    ylabel('Artificial data');
+end
